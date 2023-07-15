c087500ec0d5f0884e19bd8c7ac2477a8d79a20b
Takato Ueno <naf@auriga.dev>
2023-07-16 00:30:20 +0900

    いっぱい直した

--- src/ShareComponent.js
+++ src/ShareComponent.js
@@ -1,3 +1,4 @@
+/* global chrome */
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -10,6 +11,7 @@ import {
     BsGithub,
 } from 'react-icons/bs';
 import packageJson from '../package.json';
+import './index.css';
 
 function ShareComponent() {
     const { handleAddNewInstance } = useAddNewInstance();
@@ -22,7 +24,6 @@ function ShareComponent() {
         const savedInstancesList = localStorage.getItem('instancesList');
         return savedInstancesList ? JSON.parse(savedInstancesList) : [];
     });
-    const [versionError] = useState(null);
     const [addInstanceError, setAddInstanceError] = useState(null);
     const [successMessage, setSuccessMessage] = useState(null);
 
@@ -38,11 +39,12 @@ function ShareComponent() {
             localStorage.getItem('preferredInstance');
         if (savedPreferredInstance) {
             setMastodonInstance(savedPreferredInstance);
+        }else if(instancesList.length > 0){
+            setMastodonInstance(instancesList[0])
         }
-        setCombinedValue('');
-
-        if (!savedInstances || savedInstances.length === 0) setShowAddForm(true);
-    }, [location]);
+        if (!savedInstances || savedInstances.length === 0)
+            setShowAddForm(true);
+    }, [location, instancesList]);
 
     const handleAddInstance = () => {
         handleAddNewInstance(
@@ -56,6 +58,8 @@ function ShareComponent() {
         setMastodonInstance(newMastodonInstance);
         // フォームの入力フィールドをクリアする
         setNewMastodonInstance('');
+        // MastodonインスタンスのURLをローカルストレージに保存
+        localStorage.setItem('instancesList', JSON.stringify(instancesList));
     };
 
     useEffect(() => {
@@ -63,34 +67,50 @@ function ShareComponent() {
     }, [instancesList]);
 
     const handleShare = () => {
-        if (!versionError) {
-            // MastodonインスタンスのURLをローカルストレージに保存
-            localStorage.setItem(
-                'instancesList',
-                JSON.stringify(instancesList),
-            );
+        // 指定したURLにジャンプ
+        const shareText = encodeURIComponent(combinedValue);
+        window.open(`https://${mastodonInstance}/share?text=${shareText}`, '_blank');
+    };
 
-            // 指定したURLにジャンプ
-            const shareText = encodeURIComponent(combinedValue);
-            window.location.href = `https://${mastodonInstance}/share?text=${shareText}`;
-        }
+    const handleButtonClick = () => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const activeTab = tabs[0];
+            const title = activeTab.title;
+            const url = activeTab.url;
+            setCombinedValue(`Title: ${title}\nURL: ${url}`);
+            console.log(combinedValue);
+        });
+    };
+
+    const handleCopyToClipboard = async () => {
+        await navigator.clipboard.writeText(combinedValue);
     };
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <div className="max-w-md w-full bg-white p-4 rounded-md shadow-md">
+        <div className="items-center main-container">
+            <div className="p-4">
                 <h2 className="text-2xl font-bold flex items-center">
                     <BsPencilFill className="mr-1" />
                     シェアする内容
                 </h2>
-
                 <textarea
-                    className="w-full mt-2 p-2 border rounded-md"
+                    className="text-base w-full mt-2 p-2 border rounded-md"
                     rows={6}
                     value={combinedValue}
                     onChange={(e) => setCombinedValue(e.target.value)}
                 />
-
+                <button
+                    onClick={handleButtonClick}
+                    className="mt-2 mb-2 py-2 px-4 mr-2 bg-lime-500 text-white rounded"
+                >
+                    現在のページを取得
+                </button>
+                <button
+                    onClick={handleCopyToClipboard}
+                    className="mt-2 mb-2 py-2 px-4 bg-orange-500 text-white rounded"
+                >
+                    クリップ ボードにコピー
+                </button>
                 {instancesList.length > 0 ? (
                     <>
                         <h2 className="text-lg mt-4 flex items-center">
@@ -99,7 +119,7 @@ function ShareComponent() {
                         </h2>
 
                         <select
-                            className="w-full mt-2 p-2 border rounded-md"
+                            className="text-base w-full mt-2 p-2 border rounded-md"
                             value={mastodonInstance}
                             onChange={(e) =>
                                 setMastodonInstance(e.target.value)
@@ -113,14 +133,14 @@ function ShareComponent() {
                         </select>
                         {!showAddForm ? (
                             <p
-                                className="mt-4 mb-2 text-blue-600 cursor-pointer hover:underline"
+                                className="text-base mt-4 mb-2 text-blue-600 cursor-pointer hover:underline"
                                 onClick={() => setShowAddForm(true)}
                             >
                                 リストにありませんか？
                             </p>
                         ) : (
                             <p
-                                className="mt-4 mb-2 text-blue-600 cursor-pointer hover:underline"
+                                className="text-base mt-4 mb-2 text-blue-600 cursor-pointer hover:underline"
                                 onClick={() => setShowAddForm(false)}
                             >
                                 フォームを隠す
@@ -128,7 +148,6 @@ function ShareComponent() {
                         )}
                     </>
                 ) : null}
-
                 {showAddForm && (
                     <>
                         <h3 className="text-lg flex items-center">
@@ -137,7 +156,7 @@ function ShareComponent() {
                         </h3>
                         <div className="flex justify-between mt-2">
                             <input
-                                className="flex-grow mr-2 p-2 border rounded-md"
+                                className="text-base flex-grow mr-2 p-2 border rounded-md"
                                 placeholder="mastodon.social"
                                 value={newMastodonInstance}
                                 onChange={(e) =>
@@ -153,7 +172,6 @@ function ShareComponent() {
                         </div>
                     </>
                 )}
-
                 {addInstanceError && (
                     <p className="text-red-500">{addInstanceError}</p>
                 )}
@@ -161,13 +179,13 @@ function ShareComponent() {
                     <p className="text-green-500">{successMessage}</p>
                 )}
                 <button
-                    className="w-full mt-4 p-2 bg-blue-500 text-white rounded-md"
+                    className="text-base w-full mt-4 p-2 bg-blue-500 text-white rounded-md"
                     onClick={handleShare}
                 >
                     共有
                 </button>
                 <Link to="/settings">
-                    <p className="mt-2 text-gray-500 hover:underline flex items-center">
+                    <p className="text-base mt-2 text-gray-500 hover:underline flex items-center">
                         <BsGearFill className="mr-1" />
                         設定
                     </p>
